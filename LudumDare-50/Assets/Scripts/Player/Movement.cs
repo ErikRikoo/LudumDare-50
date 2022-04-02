482040b8b5143a518be4335f3c2536db12b7cd04
benoitLemoine <lemo.benoit@gmail.com>
2022-04-02 16:00:08 +0200

    [ADD] Basic player movements

--- LudumDare-50/Assets/Scripts/Player/Movement.cs
+++ LudumDare-50/Assets/Scripts/Player/Movement.cs
@@ -5,9 +5,24 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    public void OnMovement(InputAction.CallbackContext context)
+    [SerializeField] private GameObject m_target;
+    [SerializeField] private float m_Speed = 1f;
+    [Range(1, 10)]
+    [SerializeField] private float m_Damping;
+
+    private Vector2 m_TargetMovement;
+    private Vector2 m_Movement;
+
+    public void OnMovement(InputAction.CallbackContext _context)
     {
-        Vector2 movement = context.ReadValue<Vector2>();
-        Debug.Log(movement);
+        m_TargetMovement = _context.ReadValue<Vector2>();
+    }
+
+
+    private void Update()
+    {
+        m_Movement = Vector2.Lerp(m_Movement, m_TargetMovement, m_Damping * Time.deltaTime);
+        Vector2 planeMovement = m_Movement * (Time.deltaTime * m_Speed);
+        m_target.transform.Translate(planeMovement.x, 0, planeMovement.y, Space.World);
     }
 }
