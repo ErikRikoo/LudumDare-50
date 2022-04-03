4e2e30914668104573a812cfaf21cdbfa95f3378
Erik Kubiak <erikkubiak@gmail.com>
2022-04-03 11:57:22 +0200

    Adding tap tap dans tes mains

--- LudumDare-50/Assets/Scripts/Environnement/WaterHandling/TankerTap.cs
+++ LudumDare-50/Assets/Scripts/Environnement/WaterHandling/TankerTap.cs
@@ -5,11 +5,17 @@ namespace Environnement
 {
     public class TankerTap : MonoBehaviour
     {
+        [SerializeField] private TankerTapCollection m_Collection;
+        
         [SerializeField] private WaterFillable m_Fillable;
+        private bool m_IsBlocked;
+
+        public bool IsBlocked => m_IsBlocked;
 
         private void Awake()
         {
             StartFilling();
+            m_Collection.AddElement(this);
         }
 
         public void StartFilling()
@@ -20,6 +26,7 @@ namespace Environnement
         public void StopFilling()
         {
             m_Fillable.StopFilling();
+            m_IsBlocked = true;
         }
     }
 }
---
+++ LudumDare-50/Assets/Scripts/Environnement/WaterHandling/TankerTapCollection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Environnement
+{
+    [CreateAssetMenu(fileName = "TankerTapCollection", menuName = "Water/Tanker Tap Collection", order = 0)]
+    public class TankerTapCollection : ScriptableObject, IEnumerable<TankerTap>
+    {
+        private List<TankerTap> m_Elements;
+
+        public void AddElement(TankerTap _tap)
+        {
+            m_Elements.Add(_tap);
+        }
+
+        public IEnumerable<TankerTap> GetValidTankers()
+        {
+            return this.Where(element => !element.IsBlocked);
+        }
+
+        public IEnumerator<TankerTap> GetEnumerator()
+        {
+            return m_Elements.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
