0e42c1c4e02e27e170a62608fdebfeacbbaa7d06
benoitLemoine <lemo.benoit@gmail.com>
2022-04-05 00:03:53 +0200

    [ADD] Basic score manager

---
+++ LudumDare-50/Assets/Scripts/Utilities/ScoreManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityAtoms.BaseAtoms;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField] FloatVariable m_ScoreVariable;
+    [SerializeField] VoidEvent m_StartEvent;
+    [SerializeField] VoidEvent m_StopEvent;
+    [SerializeField]  m_ScoreTextMesh;
+
+    private float m_StartTime;
+    private float m_EndTime;
+
+    private void Start()
+    {
+        m_StartEvent.Register(StartTimer);
+        m_StopEvent.Register(StopTimer);
+    }
+
+    private void StartTimer()
+    {
+        m_StartTime = Time.time;
+    }
+
+    private void StopTimer()
+    {
+        m_EndTime = Time.time;
+        m_ScoreVariable.Value = m_EndTime - m_StartTime;
+        m_ScoreTextMesh.text = "Score : " + m_ScoreVariable;
+
+    }
+}
