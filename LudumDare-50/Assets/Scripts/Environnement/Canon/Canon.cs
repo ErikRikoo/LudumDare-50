b31bfc8e2e42153e5443257af80b5ef9d5773808
NocTua <noctua0609@gmail.com>
2022-04-04 23:01:41 +0200

    [MOD] anim canon

--- LudumDare-50/Assets/Scripts/Environnement/Canon/Canon.cs
+++ LudumDare-50/Assets/Scripts/Environnement/Canon/Canon.cs
@@ -6,12 +6,19 @@ namespace Environnement
     public class Canon : MonoBehaviour
     {
         [SerializeField] private Transform lanceur;
+        private Collider objet_to_eject;
         private void OnTriggerEnter(Collider other)
         {
             if (other.TryGetComponent(out IEjectable putInCanon))
             {
+                objet_to_eject = other;
                 putInCanon.OnPutIn(this,lanceur);
             }
+        }
+
+        public void eject_object()
+        {
+            objet_to_eject.GetComponent<Eject_object>().byebye_object();
         }
     }
 }
--- LudumDare-50/Assets/Scripts/Environnement/Canon/Eject_object.cs
+++ LudumDare-50/Assets/Scripts/Environnement/Canon/Eject_object.cs
@@ -9,22 +9,26 @@ public class Eject_object : MonoBehaviour,IEjectable
     
     [SerializeField] private float P_canon = 200;
     [SerializeField] private ColliderEvent Taken;
-    [SerializeField] private float time_delay = 0;
+    //[SerializeField] private float time_delay = 0;
+    private Animator m_Animator;
     private notCatchable _notCatchable;
     
     public void OnPutIn(Canon _canon,Transform lanceur)
     {
         Taken.Raise(GetComponent<Collider>());
 
+        m_Animator = _canon.GetComponent<Animator>();
         // l'objet va en position lanceur 
         transform.GetComponent<Rigidbody>().useGravity = false;
         transform.parent = lanceur;
-        transform.localPosition = Vector3.zero;
-        transform.forward = lanceur.up;
+        transform.forward = lanceur.forward;
+        transform.localPosition = Vector3.zero + (lanceur.transform.forward * GetComponent<Collider>().bounds.extents.magnitude);
+        
         _notCatchable=gameObject.AddComponent<notCatchable>();
         Debug.Log("pret à être envoyé!!!!" + transform.name + " = "+transform.forward + "= ?  " + lanceur.name + " = " +lanceur.up);
         // go animation
-        StartCoroutine(delay());
+        m_Animator.SetTrigger("Fire");
+       // StartCoroutine(delay());
 
     }
 
@@ -32,21 +36,21 @@ public class Eject_object : MonoBehaviour,IEjectable
     {
         //DO NOTHING
     }
-    private void byebye_object()
+    public void byebye_object()
     {
         
         transform.parent= null;
         transform.GetComponent<Rigidbody>().useGravity = true;
-       transform.GetComponent<Rigidbody>().AddForce((transform.forward) * P_canon);
+       transform.GetComponent<Rigidbody>().AddForce((transform.forward + transform.up) * P_canon);
        Debug.Log("bye bye PQ = " );
        Destroy(_notCatchable); // TODO : un destroy qui fonctionne  
 
     }
 
-    private IEnumerator delay()
-    {
-        yield return new WaitForSeconds(time_delay);
-        //ejection de l'objet 
-        byebye_object();
-    }
+    // private IEnumerator delay()
+    // {
+    //     yield return new WaitForSeconds(time_delay);
+    //     //ejection de l'objet 
+    //     byebye_object();
+    // }
 }
